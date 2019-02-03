1a66fc2f23b578574d1b2a105dcbbd4a9fbe92a0
parker <pwhiteford02@gmail.com>
2019-02-02 23:24:06 -0500

    finished program
    needs
    *formating
    *proper jsdoc configuration
    *look over by BDH

--- code.js
+++ code.js
@@ -7,7 +7,7 @@
 "use strict";
 const PROMPT = require('readline-sync');
 
-let yearBorn,age,amntAccidents,customerId,total;
+let yearBorn,age,agePrice,amntAccidents,customerId,total,accidentPrice,payDate;
 let firstName,lastName;
 
 /**
@@ -23,6 +23,8 @@ function main() {
     setCustomerId();
     setAtFaultAccidents();
     calcTotal();
+    paymentDate();
+    finalStatement();
 
 
 }
@@ -36,8 +38,7 @@ main();
  * **/
 
     function getFirstName() {
-   firstName = PROMPT.question(`what is your fist name`);
-        console.log(firstName);
+   firstName = PROMPT.question(`what is your first name? `);
     }
 
 /**
@@ -47,8 +48,7 @@ main();
  * **/
 
     function getLastName() {
-        lastName = PROMPT.question(`what is your last name`);
-        console.log(lastName)
+        lastName = PROMPT.question(`what is your last name? `);
 
     }
 
@@ -62,11 +62,10 @@ main();
         const THIS_YEAR = 2019;
         yearBorn = Number(PROMPT.question(`What year where you born? `));
         age = THIS_YEAR - yearBorn;
-        if (age > 115 || age < 18) {
+        if (age > 115 || age < 16) {
             console.log(`Oops it looks like you age is invalid. Please try again.`);
             return getAge();
         }
-        console.log(age);
 
 
     }
@@ -90,39 +89,73 @@ function setCustomerId() {
  **/
 
 function setAtFaultAccidents() {
-    amntAccidents = Number(PROMPT.question(`How many at fault accidents have you had in the past three years?`));
+    amntAccidents = Number(PROMPT.question(`How many at fault accidents have you had in the past three years? `));
     console.log(amntAccidents);
 
     if (amntAccidents > 20) {
-        console.log(`we're sorry it seems that you have had too many at fault accidents in the past three years. we cannot insure you `);
+        console.log(`we're sorry it seems that you have had too many at fault accidents in the past three years. we cannot insure you. `);
         return setAtFaultAccidents();
 
     }
 
-    if (amntAccidents < 0{
-        console.log(`It looks like you have entered an invalid number please try again `);
+    if (amntAccidents < 0){
+        console.log(`It looks like you have entered an invalid number please try again. `);
         return setAtFaultAccidents();
     }
 }
+
+
+/**
+ * @method
+ * @desc calculates total  payment
+ * @returns {null}
+ **/
 
 
 function calcTotal() {
-    let YOUNG_PRICE,OLD_PRICE,REALLY_OLD_PRICE;
+
    const REALLY_OLD_PRICE = 30;
    const OLD_PRICE = 10;
    const YOUNG_PRICE = 20;
+   accidentPrice = 50;
 
-    total = 0;
-    if (age > 59){
-        total = total + REALLY_OLD_PRICE;
+
+    agePrice = 0;
+
+    if (age >=60){
+        agePrice = REALLY_OLD_PRICE;
     }
     
-    if (age <) 
-    
-    if (age <= 30){
-        total = total + YOUNG_PRICE
+    if (age >= 30 && age < 45){
+        agePrice = OLD_PRICE;
     }
+    
+    if (age > 15 && age < 30){
+        agePrice = YOUNG_PRICE
+    }
+
+    total = accidentPrice * amntAccidents + agePrice;
+
 
 }
+/**
+ * @method
+ * @desc gets date of first payment
+ * @returns {null}
+ **/
 
+function paymentDate(){
+    payDate = PROMPT.question(`what date would you wish to make your first payment? (MM/DD/YY):`)
+}
+
+/**
+ * @method
+ * @desc states payment and when payment is due
+ * @returns {null}
+ **/
+
+
+function finalStatement() {
+    console.log(`Congratulations ${firstName} ${lastName} your first car insurance bill will be due on ${payDate} for $${total}.`)
+}
 
